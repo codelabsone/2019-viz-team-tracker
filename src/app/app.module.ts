fb3ea62eb6449164edd72325df7c09b928b45416
Chris Palmergit config --global user.email cwpalmer1s@yahoo.com <cwpalmer1s@yahoo.com>
2019-10-28 19:05:39 -0500

    added header background color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
