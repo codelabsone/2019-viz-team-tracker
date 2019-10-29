ee1733c85600715afb31a6c4a4d1e6c6a0732b39
NeonDactyl <treyw05@gmail.com>
2019-10-28 19:33:04 -0500

    removed duplicate import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { TeamListContainerComponent } from './team-list-container/team-list-container.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
