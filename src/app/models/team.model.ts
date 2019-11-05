40ca19a96ff1d6bbfbc288f6748bf0ec2638f6db
NHENSON <norman.henson@vizientinc.com>
2019-11-04 19:38:18 -0600

    added member avatar with 12 member cap

--- src/app/models/team.model.ts
+++ src/app/models/team.model.ts
@@ -1,13 +1,6 @@
 import { Teammember } from './member.model';
 
-export class Team{
+export class Team {
     name: string;
     members: Teammember[];
-    constructor(name: string){
-        this.name = name;
-        this.members = [];
-    }
-    addMember( member: Teammember){
-        this.members.push(member);
-    }
 }
